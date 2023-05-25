c0d14c7a8f8ebc786feffd1495116283b3ba5298
Jumpy22 <megagear2828@gmail.com>
2023-05-25 12:50:33 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementsByClassName('home')[0]
+.addEventListener('click', function (event) {
+	window.location.href = "index.html";
+});
+document.getElementsByClassName('info')[0]
+.addEventListener('click', function (event) {
+	window.location.href = "info.html";
+});
