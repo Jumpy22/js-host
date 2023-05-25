bba25cb73f9455791d8b7f5a240d6f0537922eaa
Jumpy22 <megagear2828@gmail.com>
2023-05-25 12:58:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,7 @@ document.getElementsByClassName('info')[0]
 .addEventListener('click', function (event) {
 	window.location.href = "info.html";
 });
+document.getElementsByClassName('ext')[0]
+.addEventListener('click', function (event) {
+	window.location.href = "https://www.theguardian.com/world/interactive/2013/nov/01/snowden-nsa-files-surveillance-revelations-decoded";
+});
